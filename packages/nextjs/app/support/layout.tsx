457a536ab0aaeabd87d117cb7db9f6c4fcfd5405
0xJonaseb11 <sebejaz99@gmail.com>
2026-02-28 13:50:37 +0200

    [feat]: dev support

---
+++ packages/nextjs/app/support/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Support",
+  description: "Get help with the Pipette Base Sepolia faucet. Contact support.",
+};
+
+export default function SupportLayout({ children }: { children: React.ReactNode }) {
+  return children;
+}
