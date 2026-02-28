f7ad1660e098d8c14e0abceacdee47af4031002b
Jonas Sebera <123830791+0xJonaseb11@users.noreply.github.com>
2026-02-28 00:26:13 -0500

    [docs+fix]: readme + linters (#12)
    * [docs]: readme
    
    * [docs]: readme
    
    * [docs+fix]: readme + linters

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -4,8 +4,8 @@ import React, { useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Bars3Icon } from "@heroicons/react/24/outline";
 import { Droplets } from "lucide-react";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 import { RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
@@ -32,8 +32,9 @@ export const HeaderMenuLinks = () => {
             <Link
               href={href}
               passHref
-              className={`${isActive ? "bg-secondary shadow-md" : ""
-                } hover:bg-secondary hover:shadow-md focus:!bg-secondary active:!text-neutral py-1.5 px-3 text-sm rounded-full gap-2 grid grid-flow-col`}
+              className={`${
+                isActive ? "bg-secondary shadow-md" : ""
+              } hover:bg-secondary hover:shadow-md focus:!bg-secondary active:!text-neutral py-1.5 px-3 text-sm rounded-full gap-2 grid grid-flow-col`}
             >
               {icon}
               <span>{label}</span>
